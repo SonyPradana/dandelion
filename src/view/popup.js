c535afc87446ae576da69ac23b3bfa61a686f7d5
sonypradana <sonypradana@gmail.com>
2025-11-06 21:52:52 +0700

    update agrement

--- src/view/popup.js
+++ src/view/popup.js
@@ -1,15 +1,13 @@
-import { getAgreement, setAgreement } from '../configuration';
+import { getAgreement, setAgreement } from "../configuration";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const agreeCheckbox = document.getElementById('agree-checkbox');
+document.addEventListener("DOMContentLoaded", () => {
+  const agreeCheckbox = document.getElementById("agree-checkbox");
 
-  // Set initial checkbox state
   getAgreement().then((agreed) => {
     agreeCheckbox.checked = agreed;
   });
 
-  // Listen for changes on the checkbox
-  agreeCheckbox.addEventListener('change', () => {
+  agreeCheckbox.addEventListener("change", () => {
     setAgreement(agreeCheckbox.checked);
   });
 });
