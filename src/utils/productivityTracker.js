05c748059d358c47aec37051b4efe98e44711588
sonypradana <sonypradana@gmail.com>
2026-05-28 14:26:26 +0700

    refactor: incress daily limit

--- src/utils/productivityTracker.js
+++ src/utils/productivityTracker.js
@@ -8,7 +8,7 @@ export const WEIGHT_VERSION = 1;
 export const WEIGHTS = { radio: 1, freetext: 1, dropdown: 1, formNotChecked: 5, formZen: 5 };
 export const MONTHLY_TARGET = 30_000;
 export const TARGET_MODE = 'weekly';
-export const DAILY_LIMIT = 6600;
+export const DAILY_LIMIT = 13_200;
 
 function getWeights(data) {
   return data._meta?.weights || WEIGHTS;
