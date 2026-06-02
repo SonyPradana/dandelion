4986045fbe8b98bd68d0cd7e150ced2eda7f80d8
sonypradana <sonypradana@gmail.com>
2026-06-02 21:53:01 +0700

    feat: add number input support to fill-pinned-fields

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { initializeSkriningForm } from './handlers/skriningform.js';
 import { initializeSkrining } from './handlers/skrining.js';
 import { initialize as initializeNotChecked } from './handlers/skrining-form-not-checked.js';
 import { getAgreement, getActiveConfig, getFullConfig } from './configuration.js';
+import { getFlashData } from './utils/flashSession.js';
 import { validateChain, isDailyLimitReached } from './utils/productivityTracker.js';
 import { init as quotaInit, isFeatureEnabled, isLimitReached } from './quota/quota-manager.js';
 import { controlPanel } from './components/controlPanel.js';
@@ -32,6 +33,14 @@ async function initialize() {
   const currentURL = window.location.href;
   const config = await getActiveConfig();
 
+  // const flashData = await getFlashData();
+  const flashData = {
+    pinneds: {
+      'LPM000073|FRM000265|PPM00000012|number': '120',
+      'LPM000073|FRM000265|PPM00000019|number': '80',
+    },
+  };
+
   if (
     config.notChecked?.url &&
     currentURL.includes(config.notChecked.url) &&
@@ -43,7 +52,7 @@ async function initialize() {
     currentURL.includes(config.formSkrining.url) &&
     isFeatureEnabled('skriningform')
   ) {
-    initializeSkriningForm();
+    initializeSkriningForm(flashData || {});
   } else if (
     config.skrining?.url &&
     currentURL.includes(config.skrining.url) &&
